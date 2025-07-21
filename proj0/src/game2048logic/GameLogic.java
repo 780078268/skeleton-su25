c9021fa47c26ae221fa403572446d4e7739f8117
780078268 <780078268@qq.com>
2025-07-21 17:44:36 +0800

    2048

--- proj0/src/game2048logic/GameLogic.java
+++ proj0/src/game2048logic/GameLogic.java
@@ -1,47 +1,74 @@
 package game2048logic;
 
 import game2048rendering.Side;
+
 import static game2048logic.MatrixUtils.rotateLeft;
 import static game2048logic.MatrixUtils.rotateRight;
 
 /**
- * @author  Josh Hug
+ * @author Josh Hug
  */
 public class GameLogic {
-    /** Moves the given tile up as far as possible, subject to the minR constraint.
+    /**
+     * Moves the given tile up as far as possible, subject to the minR constraint.
      *
      * @param board the current state of the board
      * @param r     the row number of the tile to move up
-     * @param c -   the column number of the tile to move up
+     * @param c     -   the column number of the tile to move up
      * @param minR  the minimum row number that the tile can land in, e.g.
      *              if minR is 2, the moving tile should move no higher than row 2.
-     * @return      if there is a merge, returns the 1 + the row number where the merge occurred.
-     *              if no merge occurs, then return minR.
+     * @return if there is a merge, returns the 1 + the row number where the merge occurred.
+     * if no merge occurs, then return minR.
      */
     public static int moveTileUpAsFarAsPossible(int[][] board, int r, int c, int minR) {
         // TODO: Fill this in in tasks 2, 3, 4
-        return 0;
+        if (board[r][c] == 0) return minR;
+        if (r == 0) return minR;
+        for (int i = r - 1; i >= minR; i--) {
+            if (board[i][c] == 0) {
+                board[i][c] = board[i + 1][c];
+                board[i + 1][c] = 0;
+                continue;
+            }
+            if (board[i][c] != 0 && board[i + 1][c] == board[i][c]) {
+                board[i][c] = 2 * board[i + 1][c];
+                board[i + 1][c] = 0;
+                return i + 1;
+            }
+            if (board[i][c] != 0 && board[i + 1][c] != board[i][c]) {
+                return minR;
+            }
+        }
+        return minR;
     }
 
     /**
      * Modifies the board to simulate the process of tilting column c
      * upwards.
      *
-     * @param board     the current state of the board
-     * @param c         the column to tilt up.
+     * @param board the current state of the board
+     * @param c     the column to tilt up.
      */
     public static void tiltColumn(int[][] board, int c) {
         // TODO: fill this in in task 5
+        for (int i = 1, minR = 0; i <= 3; i++) {
+            minR = moveTileUpAsFarAsPossible(board, i, c, minR);
+
+        }
+
         return;
     }
 
     /**
      * Modifies the board to simulate tilting all columns upwards.
      *
-     * @param board     the current state of the board.
+     * @param board the current state of the board.
      */
     public static void tiltUp(int[][] board) {
         // TODO: fill this in in task 6
+        for (int i = 0; i <= 3; i++) {
+            tiltColumn(board, i);
+        }
         return;
     }
 
@@ -55,12 +82,24 @@ public class GameLogic {
     public static void tilt(int[][] board, Side side) {
         // TODO: fill this in in task 7
         if (side == Side.NORTH) {
+            tiltUp(board);
             return;
         } else if (side == Side.EAST) {
+            rotateLeft(board);
+            tiltUp(board);
+            rotateRight(board);
             return;
         } else if (side == Side.SOUTH) {
+            rotateRight(board);
+            rotateRight(board);
+            tiltUp(board);
+            rotateLeft(board);
+            rotateLeft(board);
             return;
         } else if (side == Side.WEST) {
+            rotateRight(board);
+            tiltUp(board);
+            rotateLeft(board);
             return;
         } else {
             System.out.println("Invalid side specified");
