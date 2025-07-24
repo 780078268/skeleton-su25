0b9d7be1f62d5dd327a85ffdeb1f610159b5e08e
780078268 <780078268@qq.com>
2025-07-25 02:27:59 +0800

    new link

--- proj1a/src/LinkedListDeque61B.java
+++ proj1a/src/LinkedListDeque61B.java
@@ -1,13 +1,13 @@
 import java.util.ArrayList;
 import java.util.List;
 
-public class LinkedListDeque61B<T> implements Deque61B<T>{
+public class LinkedListDeque61B<T>implements Deque61B<T> {
     Node sentinel;
     int size;
 
     public LinkedListDeque61B() {
         size = 0;
-        sentinel = new Node(null,null,null);
+        sentinel = new Node(null, null, null);
         sentinel.next = sentinel;
         sentinel.prev = sentinel;
     }
@@ -26,18 +26,18 @@ public class LinkedListDeque61B<T> implements Deque61B<T>{
 
     @Override
     public void addFirst(T x) {
-        Node newNode = new Node(x,sentinel.next,sentinel);
+        Node newNode = new Node(x, sentinel.next, sentinel);
         sentinel.next.prev = newNode;
-        sentinel.next=newNode;
+        sentinel.next = newNode;
         size++;
     }
 
     @Override
     public void addLast(T x) {
-     Node newNode = new Node (x,sentinel,sentinel.prev);
-     sentinel.prev.next = newNode;
-     sentinel.prev = newNode;
-     size++;
+        Node newNode = new Node(x, sentinel, sentinel.prev);
+        sentinel.prev.next = newNode;
+        sentinel.prev = newNode;
+        size++;
     }
 
     @Override
@@ -53,66 +53,66 @@ public class LinkedListDeque61B<T> implements Deque61B<T>{
 
     @Override
     public boolean isEmpty() {
-        if (sentinel.next == sentinel) {
-            return true;
-        }
-        return false;
+        return size == 0;
     }
 
     @Override
     public int size() {
-        int returnSize = 0;
-        Node current = sentinel.next;
-        for(int i =0;i>=0;i++){
-            if(current.next != sentinel){
-                returnSize++;
-                current = current.next;
-            }
-            else{
-                return returnSize;
-            }
-        }
-        return returnSize;
+        return size;
     }
 
     @Override
     public T removeFirst() {
-        sentinel.next.prev = sentinel;
-        sentinel.next = sentinel.next.next;
-        size--;
-        return null;
-    }
-
-    @Override
-    public T removeLast() {
-        sentinel.prev = sentinel.prev.prev;
-        sentinel.prev.next = sentinel;
-        size--;
-        return null;
-    }
-
-    @Override
-    public T get(int index) {
-        int size = this.size;
-        Node current = sentinel.next;
-        if(index<0 || index>=size){
+        if(size == 0){
             return null;
         }
-        for(int i =0;i<index;i++){
-            sentinel = sentinel.next;
-        }
+        Node current = sentinel.next;
+        sentinel.next = current.next;
+        sentinel.next.prev = sentinel;
+        size--;
         return current.item;
     }
 
     @Override
-    public T getRecursive(int index) {
-        int size = this.size;
-        Node current = sentinel.next;
-        for(int i =0;i<index;i++){
-            current.next = current.prev;
-            current.prev = current;
-            current = current.next;
+    public T removeLast() {
+        if(size == 0){
+            return null;
         }
-        return null;
+        Node current = sentinel.prev;
+        sentinel.prev = sentinel.prev.prev;
+        sentinel.prev.next = sentinel;
+        size--;
+        return current.item;
+    }
+
+    @Override
+    public T get(int index) {
+        Node current = sentinel.next;
+        if (index < 0 || index >= size) {
+            return null;
+        } else {
+            for (int i = 0; i < index; i++) {
+                current = current.next;
+            }
+            return current.item;
+        }
+    }
+    private T recursive(int index,Node current) {
+        current = current.next;
+        if (index < 0 || index >= size) {
+            return null;
+        }
+        if(index == 0){
+            return current.item;
+        }
+        else{
+            index--;
+            return recursive(index,current);
+        }
+
+    }
+    @Override
+    public T getRecursive(int index) {
+        return recursive(index,sentinel);
     }
 }
